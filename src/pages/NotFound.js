492caa17c9670c79cf378ea64f273be9ffde106f
Robert <osipovitchr@gmail.com>
2022-05-03 13:20:42 -0400

    Prompt from router to prevent unwanted transitions

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className="centered">
+        <p>404 Page not Found</p>
+    </div>
+  )
+}
+
+export default NotFound
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import { useParams, Route } from 'react-router-dom'
+import HighlightedQuote from '../components/quotes/HighlightedQuote'
 import Comments from '../components/comments/Comments'
+
+const dummy_quotes = [
+  {id: 'q1', author: 'Rob', text: 'blahblahblah'},
+  {id: 'q2', author: 'bob', text: 'blahblahblah'},
+  {id: 'q3', author: 'jim', text: 'blahblahblah'}
+]
 
 const QuoteDetail = () => {
     const params = useParams()
+
+    const quote = dummy_quotes.find((quote) => {
+        return quote.id === params.quoteId
+    })
+
+    if (!quote) {return <p>No quote found</p>}
   return (
       <>
-        <h1>QuoteDetail Page</h1>
-        <p>{params.quoteId}</p>
+       <HighlightedQuote text={quote.text} author={quote.author} />
         <Route path={`/quotes/${params.quoteId}/comments`}>
             <Comments />
         </Route>
